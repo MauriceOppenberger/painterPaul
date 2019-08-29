de852109e0ed95def348b6fca4910b2f01c18aec
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2019-08-28 22:47:28 -0400

    lowercase

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 
-import SEO from "../components/SEO"
+import SEO from "../components/seo"
 
 import { PageWrapper } from "../templates/styles/Page"
 
